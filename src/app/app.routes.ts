d1afa44db8ce5705621d86e2b3f27635994120cc
nasseraitahmed07 <aitahmednasser@outlook.com>
2025-10-23 21:13:41 +0200

    Scrum 11 upload files (#4)
    upload files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {TemplateComponent} from "./core/template/container/template.component";
 import {ScpiCatalogComponent} from "./features/scpi-catalog/scpi-catalog.component";
+import { UploadFileComponent } from './features/upload-file/upload-file.component';
 
 export const routes: Routes = [
 
@@ -8,7 +9,8 @@ export const routes: Routes = [
     path: '', component: TemplateComponent, children: [
       {
         path: 'scpi', component: ScpiCatalogComponent
-      }
+      },
+      { path: 'profile', component: UploadFileComponent },
     ],
   }
 
