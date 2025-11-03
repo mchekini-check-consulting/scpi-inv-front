d4d1de0876c9dec5ea354afc08e9fd8c56a802f6
lehara <44174926+lyeslehara1996@users.noreply.github.com>
2025-11-04 00:50:24 +0100

    Display Scpi Details
    i

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import {Routes} from '@angular/router';
 import {TemplateComponent} from "./core/template/container/template.component";
 import {ScpiCatalogComponent} from "./features/scpi-catalog/scpi-catalog.component";
-import {AuthGuard} from './core/guards/auth.guard';
+import { AuthGuard } from './core/guards/auth.guard';
 import {HomeComponent} from "./core/template/home/home.component";
-import {UploadFileComponent} from './features/upload-file/upload-file.component';
+import { UploadFileComponent } from './features/upload-file/upload-file.component';
+import { ScpiInvestComponent } from './features/scpi-invest/scpi-invest.component';
+import { ScpiDetailComponent } from './features/scpi-detail/scpi-detail.component';
 import {HistoryComponent} from "./features/history/history.component";
-import {ScpiInvestComponent} from './features/scpi-invest/scpi-invest.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,6 +17,7 @@ export const routes: Routes = [
     children: [
       { path: 'scpi', component: ScpiCatalogComponent },
       { path: 'scpi/:id/invest', component: ScpiInvestComponent },
+      { path: 'scpi/:slug', component: ScpiDetailComponent },
       { path: 'profile', component: UploadFileComponent },
       { path: 'history', component: HistoryComponent }
     ]
--- src/app/core/template/components/scpi-card/scpi-card.component.ts
+++ src/app/core/template/components/scpi-card/scpi-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Scpi } from '../../../../models/scpi.model';
+import { Scpi, ScpiDetail } from "../../../../models/scpi.model";
 import { Router, RouterLink } from '@angular/router';
 
 
@@ -13,34 +13,18 @@ import { Router, RouterLink } from '@angular/router';
 })
 export class ScpiCardComponent {
   @Input() scpi!: Scpi;
-  
-  constructor(private router: Router) {}
+  @Input() ScpiDetail !: ScpiDetail;
+  @Output() viewMore = new EventEmitter<void>();
+  @Output() invest = new EventEmitter<void>();
 
-  onViewMore(): void {
-    console.log("--->", this.scpi);
-    
-  }
+  constructor(private router: Router){}
+onViewMore(): void {
+  const slug = `${this.scpi.name}-${this.scpi.manager}`;
+  this.router.navigate([`/dashboard/scpi/${slug}`]);
+}
+
 
   onInvest(): void {
     this.router.navigate(['/dashboard/scpi', this.scpi.id, 'invest']);
   }
-
-getCountryCode(country: string): string {
-  const codes: { [key: string]: string } = {
-    'France': 'FR',
-    'Pays-Bas': 'NL',
-    'Allemagne': 'DE',
-    'Espagne': 'ES',
-    'Italie': 'IT',
-    'Belgique': 'BE',
-    'Portugal': 'PT',
-    'Luxembourg': 'LU',
-    'Suisse': 'CH',
-    'Royaume-Uni': 'GB',
-    'Irlande': 'IE',
-    'Europe': 'EU',
-  };
-  
-  return codes[country] || 'EU';
-}
 }
