28a79a8667a4c5073c20bf3e7a9d0fe7bfedafdc
nelab <naoufal.el.abouyi@pwc.com>
2025-11-04 00:36:16 +0100

    fix service and history table and add a front filter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import {Routes} from '@angular/router';
 import {TemplateComponent} from "./core/template/container/template.component";
 import {ScpiCatalogComponent} from "./features/scpi-catalog/scpi-catalog.component";
-import { AuthGuard } from './core/guards/auth.guard';
+import {AuthGuard} from './core/guards/auth.guard';
 import {HomeComponent} from "./core/template/home/home.component";
-import { UploadFileComponent } from './features/upload-file/upload-file.component';
-import { ScpiInvestComponent } from './features/scpi-invest/scpi-invest.component';
-import { ScpiDetailComponent } from './features/scpi-detail/scpi-detail.component';
+import {UploadFileComponent} from './features/upload-file/upload-file.component';
+import {ScpiInvestComponent} from './features/scpi-invest/scpi-invest.component';
+import {ScpiDetailComponent} from './features/scpi-detail/scpi-detail.component';
 import {HistoryComponent} from "./features/history/history.component";
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+
   {
     path: 'dashboard',
     component: TemplateComponent,
@@ -20,6 +20,7 @@ export const routes: Routes = [
       { path: 'scpi/:slug', component: ScpiDetailComponent },
       { path: 'profile', component: UploadFileComponent },
       { path: 'history', component: HistoryComponent }
-    ]
+
+    ],
   }
 ];
--- src/app/models/request-history.model.ts
+++
@@ -1,6 +0,0 @@
-export interface RequestHistory {
-  id: number;
-  date: string;
-  status: string;
-
-}
