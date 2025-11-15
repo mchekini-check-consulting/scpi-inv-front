8fd70b2e5dd41eb246a541aeb781ae10d8a9f442
Meziani Belkacem <belkacem.meziani@corepulse.dz>
2025-11-15 20:23:33 +0100

    add permissionsApiUrl in environement prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://prod.scpi-inv.check-consulting.net',
+  permissionsApiUrl: 'https://prod.scpi-inv.check-consulting.net',
   environmentName: 'production'
 };
